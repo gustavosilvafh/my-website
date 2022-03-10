a75a6e345554d1157ca866989f09fbe82dcb9eeb
Gustavo Silva <gustavofsilva@email.com>
2022-03-10 19:48:29 -0300

    added copy

--- src/shared/components/footer/footer.tsx
+++ src/shared/components/footer/footer.tsx
@@ -1,9 +1,9 @@
-import { Flex, Link } from "@chakra-ui/react"
+import { Flex, Link, Text } from "@chakra-ui/react"
 import { FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaWhatsapp } from "react-icons/fa"
 
 export const Footer = () => {
     return (
-        <Flex position={"absolute"} bottom="0" fontSize={20} py={5} >
+        <Flex position={"absolute"} bottom="0" fontSize={20} py={5} justify={"space-evenly"} w={"90%"} align={"center"}>
             <Flex gap={2}>
                 <Link href="https://www.instagram.com/gustavosilvafh/" isExternal opacity={0.6} _hover={{ opacity: 1 }}>
                     <FaInstagram />
@@ -20,6 +20,9 @@ export const Footer = () => {
                 <Link href="https://api.whatsapp.com/send?1=pt_BR&phone=5542988775252" isExternal opacity={0.6} _hover={{ opacity: 1 }}>
                     <FaWhatsapp />
                 </Link>
+            </Flex>
+            <Flex>
+                <Text fontSize={14}>2022 © Todos os direitos reservados.</Text>
             </Flex>
         </Flex>
     )
